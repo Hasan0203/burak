c52ee507a7c51c43c89382a590cb7fa9d88a71b8
Kevin <makhmudovkhasan@gmail.com>
2024-02-28 00:48:21 +0900

    feat: new TASK-H

--- src/train.ts
+++ src/train.ts
@@ -1,7 +1,15 @@
-function getReverse(str: string) {
-    return str.split('').reverse().join('');
+// TASK - H
+
+function getPositive(arr: number[]) {
+    let posNum: number[] = arr.filter(num => num > 0);
+    
+    let natija = posNum.join('');
+
+    return natija; 
 }
 
-console.log(getReverse("hello"));
+
+console.log(getPositive([1, -4, 2]));
+
 
 
