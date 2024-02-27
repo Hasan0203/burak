9b56d6b09dc416285b89565cd5fc6c2bcbdca938
Kevin <makhmudovkhasan@gmail.com>
2024-02-27 12:45:32 +0900

    feat: integration of environmental variables

--- train.js
+++ train.js
@@ -1,21 +1,2 @@
-// TASK -G
 
-function indexOfMax(arr) {
-    if (arr.length === 0) {
-        return -1;
-    }
-
-    let max = arr[0];
-    let maxIndex = 0;
-
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > max) {
-            maxIndex = i;
-            max = arr[i];
-        }
-    }
-
-    return maxIndex;
-}
-console.log(indexOfMax([5, 21, 12, 21, 8]));
 
