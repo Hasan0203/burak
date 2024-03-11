277b96900d36ca02776af9e6b4722f9a8aa5d000
Kevin <makhmudovkhasan@gmail.com>
2024-03-11 16:58:32 +0900

    feat: TASK-K

--- src/train.ts
+++ src/train.ts
@@ -4,6 +4,21 @@
  selfDestroy
  */
 
+ // TASK -K
+
+ function countVowels(str: string): number {
+    let count = 0;
+    const vowels = "aeiouAEIOU";
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countVowels("Kevin"));
+
 
 /**
  Traditional FD (Frontend Development) => BSSR (Admin)=> EJS framework orqali 
