82f854fc225ace14bdb2c882ea331478851499e9
Kevin <makhmudovkhasan@gmail.com>
2024-03-13 02:44:33 +0900

    feat: locals variable integration

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -6,10 +6,9 @@ import ProductService from "../models/Product.service";
 const productService = new ProductService();
 
 const productController: T = {};
-
 productController.getAllProducts = async (req: Request, res: Response) => {
     try {
-        console.log("getAllProducts");   
+        console.log("getAllProducts");  
         res.render("products");
     } catch (err) {
         console.log("Error, getAllProducts:", err);
