044d306575a2a0b1449ed06be242bc8ee05f1b23
Kevin <makhmudovkhasan@gmail.com>
2024-03-13 10:11:13 +0900

    feat: TASK-L

--- src/train.ts
+++ src/train.ts
@@ -1,26 +1,10 @@
 
-/**
- request join
- selfDestroy
- */
+ // TASK -L
 
- // TASK -K
+ function reverseSentence(sentence: string): string {
+    const words = sentence.split(" ");
+    const reversedWords = words.map(word => word.split("").reverse().join(""));
+    return reversedWords.join(" ");
+ }
+console.log(reverseSentence("we like coding!"));
 
- function countVowels(str: string): number {
-    let count = 0;
-    const vowels = "aeiouAEIOU";
-    for (let i = 0; i < str.length; i++) {
-        if (vowels.includes(str[i])) {
-            count++;
-        }
-    }
-    return count;
-}
-
-console.log(countVowels("Kevin"));
-
-
-/**
- Traditional FD (Frontend Development) => BSSR (Admin)=> EJS framework orqali 
- Modern FD (Frontend Development) => SPA (Users' app) => REACT library
- */
