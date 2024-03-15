94ef9e911e446e382c5d4188508308cb1a6f986f
Kevin <makhmudovkhasan@gmail.com>
2024-03-16 05:54:13 +0900

    fix: modify updateChosenProduct business logic

--- src/libs/types/product.ts
+++ src/libs/types/product.ts
@@ -27,3 +27,16 @@ export interface ProductInput {
     productImages?: string[];
     productViews?: number;
 }
+
+export interface ProductUpdateInput {
+    productStatus?: ProductStatus;
+    productCollection?: ProductCollection;
+    productName?: string;
+    productPrice?: number;
+    productLeftCount?: number;
+    productSize?: ProductSize;
+    productVolume?: number;  
+    productDesc?: string;
+    productImages?: string[];
+    productViews?: number;
+}
