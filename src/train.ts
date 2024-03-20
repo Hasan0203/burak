b8f3a3d9baf9b600ca7535c52e7dbfa5cd131acf
Kevin <makhmudovkhasan@gmail.com>
2024-03-21 06:56:19 +0900

    feat: TASK-O

--- src/train.ts
+++ src/train.ts
@@ -1,6 +1,19 @@
 
  // TASK - O
- 
+ function calculateSumOfNumbers(arr: any[]): number {
+    let sum: number = 0;
+
+    for (const item of arr) {
+        if (typeof item === 'number') {
+            sum += item;
+        }
+    }
+
+    return sum;
+}
+
+console.log(calculateSumOfNumbers([10, "10", {son: 10}, true, 35]));
+
 
 
 
