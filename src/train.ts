12c4cab27d734e9825df92de526df8185ef4176a
Kevin <makhmudovkhasan@gmail.com>
2024-03-23 13:17:06 +0900

    feat: TASK-P

--- src/train.ts
+++ src/train.ts
@@ -1,5 +1,12 @@
 
- // TASK - 
+ // TASK - P
+
+ function objectToArray(obj: { [key: string]: number }): [string, number][] {
+    return Object.entries(obj);
+}
+
+console.log(objectToArray({a: 10, b: 20}));
+
 
 
 
